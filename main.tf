9c29b05923e8f1542f500d94a9b225c82910ed9a
Thomas Garcia <thomasakulla@gmail.com>
2025-09-25 05:25:19 -0400

    hotfix8

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ terraform {
   backend "s3" {
     bucket = "my-terraform-state-bucket" 
     key = "prod/terraform.tfstate"
-    region = "us-east-2" 
+    region = "us-east-1" 
     encrypt = true 
   }
 }
